3ca00cbeb3d4e678bff27968f9343885759aaa5b
Sagar Khan <sagar.cmt1920@gmail.com>
2023-10-03 23:11:46 +0600

    index file

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
+});
+
+Route::get('info',function (){
+    return view ('info');
+});
+
+
+
+Route::get('/sagar_khan', function (){
+    echo "<h1>Sagar Khan</h1>";
 });
